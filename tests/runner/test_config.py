78b2c300ba24e48c7108058835f09d666232b49c
Lester Hedges <lester.hedges@gmail.com>
2023-10-30 12:26:31 +0000

    Add more tests for dynamics options.

--- tests/runner/test_config.py
+++ tests/runner/test_config.py
@@ -1,12 +1,12 @@
-import sire as sr
 import tempfile
+import sire as sr
 
 from somd2.config import Config
 from somd2.runner import Runner
 
 
-def test_config():
-    """Validate that default options are set correctly."""
+def test_dynamics_options():
+    """Validate that dynamics options are set correctly."""
 
     with tempfile.TemporaryDirectory() as tmpdir:
         # Load the demo stream file.
@@ -27,6 +27,7 @@ def test_config():
         # Store the dynamics object.
         d = runner._sim._dyn
 
+        # Timestep.
         assert config_inp.equilibration_timestep == d.timestep()
 
         # Setup a dynamics object for production.
@@ -35,15 +36,35 @@ def test_config():
         # Store the dynamics object.
         d = runner._sim._dyn
 
+        # Timestep.
         assert str(config_inp.timestep).lower() == str(d.timestep()).lower()
-        assert (
-            str(config_inp.temperature).lower()
-            == str(d.ensemble().temperature()).lower()
-        )
-        assert str(config_inp.pressure).lower() == str(d.ensemble().pressure()).lower()
+
+        # Schedule.
         assert (
             config_inp.lambda_schedule.to_string().lower()
             == d.get_schedule().to_string().lower()
         )
+
+        # Cutoff-type.
         assert config_inp.cutoff_type.lower() == d.info().cutoff_type().lower()
+
+        # Platform.
         assert config_inp.platform.lower() == d.platform().lower()
+
+        # Temperature and pressure.
+        if not d.ensemble().is_micro_canonical():
+            assert (
+                str(config_inp.temperature).lower()
+                == str(d.ensemble().temperature()).lower()
+            )
+            assert (
+                str(config_inp.pressure).lower() == str(d.ensemble().pressure()).lower()
+            )
+
+        # Constraint.
+        assert config_inp.constraint.lower() == d.constraint().lower()
+
+        # Integrator.
+        assert config_inp.integrator.lower().replace(
+            "_", ""
+        ) == d.integrator().__class__.__name__.lower().replace("integrator", "")
