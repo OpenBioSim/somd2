cc2df101451c1037c9517a7619b2fb4982928d66
Lester Hedges <lester.hedges@gmail.com>
2024-07-17 16:09:49 +0100

    Use staticmethod for calculating the charge difference. [ci skip]

--- tests/runner/test_alchemical_ions.py
+++ tests/runner/test_alchemical_ions.py
@@ -9,23 +9,14 @@ def test_alchemical_ions(ethane_methanol):
     # Clone the system.
     mols = ethane_methanol.clone()
 
-    # Helper function to return the charge difference between the end states.
-    def charge_difference(mols):
-        from sire import morph
-
-        reference = morph.link_to_reference(mols)
-        perturbed = morph.link_to_perturbed(mols)
-
-        return (perturbed.charge() - reference.charge()).value()
-
     # Add 10 Cl- ions.
     new_mols = Runner._create_alchemical_ions(mols, 10)
 
     # Make sure the charge difference is correct.
-    assert math.isclose(charge_difference(new_mols), -10.0, rel_tol=1e-6)
+    assert math.isclose(Runner._get_charge_difference(new_mols), -10.0, rel_tol=1e-6)
 
     # Add 10 Na+ ions.
     new_mols = Runner._create_alchemical_ions(mols, -10)
 
     # Make sure the charge difference is correct.
-    assert math.isclose(charge_difference(new_mols), 10.0, rel_tol=1e-6)
+    assert math.isclose(Runner._get_charge_difference(new_mols), 10.0, rel_tol=1e-6)
