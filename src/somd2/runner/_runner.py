8a937bb908d6878f7029121436d8ea5b5909fd7c
Lester Hedges <lester.hedges@gmail.com>
2025-02-11 11:32:49 +0000

    Fix equilibration_timestep kwarg name.

--- src/somd2/runner/_runner.py
+++ src/somd2/runner/_runner.py
@@ -387,7 +387,7 @@ class Runner(_RunnerBase):
                         "device": device,
                         "lambda_value": lambda_value,
                         "rest2_scale": rest2_scale,
-                        "timestep": self._config.equilibrium_timestep,
+                        "timestep": self._config.equilibration_timestep,
                         "constraint": (
                             "none"
                             if not self._config.equilibration_constraints
